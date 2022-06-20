a076be296d264f8b45454798b555d3b375008089
Jack Works <jackworks@protonmail.com>
2022-06-20 14:33:47 +0800

    chore: add EventTarget types

--- packages/plugin-types/types/web.d.ts
+++ packages/plugin-types/types/web.d.ts
@@ -1,6 +1,14 @@
 // This file is generated from @types/web
 
 
+/////////////////////////////
+/// Window APIs
+/////////////////////////////
+interface AddEventListenerOptions extends EventListenerOptions {
+    once?: boolean;
+    passive?: boolean;
+    signal?: AbortSignal;
+}
 interface AesCbcParams extends Algorithm {
     iv: BufferSource;
 }
@@ -40,6 +48,14 @@ interface EcdhKeyDeriveParams extends Algorithm {
 }
 interface EcdsaParams extends Algorithm {
     hash: HashAlgorithmIdentifier;
+}
+interface EventInit {
+    bubbles?: boolean;
+    cancelable?: boolean;
+    composed?: boolean;
+}
+interface EventListenerOptions {
+    capture?: boolean;
 }
 interface HkdfParams extends Algorithm {
     hash: HashAlgorithmIdentifier;
@@ -114,6 +130,25 @@ interface TextEncoderEncodeIntoResult {
     read?: number;
     written?: number;
 }
+interface AbortSignalEventMap {
+    "abort": Event;
+}
+/** A signal object that allows you to communicate with a DOM request (such as a Fetch) and abort it if required via an AbortController object. */
+interface AbortSignal extends EventTarget {
+    /** Returns true if this AbortSignal's AbortController has signaled to abort, and false otherwise. */
+    readonly aborted: boolean;
+    onabort: ((this: AbortSignal, ev: Event) => any) | null;
+    readonly reason: any;
+    throwIfAborted(): void;
+    addEventListener<K extends keyof AbortSignalEventMap>(type: K, listener: (this: AbortSignal, ev: AbortSignalEventMap[K]) => any, options?: boolean | AddEventListenerOptions): void;
+    addEventListener(type: string, listener: EventListenerOrEventListenerObject, options?: boolean | AddEventListenerOptions): void;
+    removeEventListener<K extends keyof AbortSignalEventMap>(type: K, listener: (this: AbortSignal, ev: AbortSignalEventMap[K]) => any, options?: boolean | EventListenerOptions): void;
+    removeEventListener(type: string, listener: EventListenerOrEventListenerObject, options?: boolean | EventListenerOptions): void;
+}
+declare var AbortSignal: {
+    prototype: AbortSignal;
+    new (): AbortSignal;
+};
 /** Basic cryptography features available in the current context. It allows access to a cryptographically strong random number generator and to cryptographic primitives. */
 interface Crypto {
     /** Available only in secure contexts. */
@@ -139,6 +174,88 @@ interface CryptoKey {
 declare var CryptoKey: {
     prototype: CryptoKey;
     new (): CryptoKey;
+};
+/** An event which takes place in the DOM. */
+interface Event {
+    /** Returns true or false depending on how event was initialized. True if event goes through its target's ancestors in reverse tree order, and false otherwise. */
+    readonly bubbles: boolean;
+    cancelBubble: boolean;
+    /** Returns true or false depending on how event was initialized. Its return value does not always carry meaning, but true can indicate that part of the operation during which event was dispatched, can be canceled by invoking the preventDefault() method. */
+    readonly cancelable: boolean;
+    /** Returns true or false depending on how event was initialized. True if event invokes listeners past a ShadowRoot node that is the root of its target, and false otherwise. */
+    readonly composed: boolean;
+    /** Returns the object whose event listener's callback is currently being invoked. */
+    readonly currentTarget: EventTarget | null;
+    /** Returns true if preventDefault() was invoked successfully to indicate cancelation, and false otherwise. */
+    readonly defaultPrevented: boolean;
+    /** Returns the event's phase, which is one of NONE, CAPTURING_PHASE, AT_TARGET, and BUBBLING_PHASE. */
+    readonly eventPhase: number;
+    /** Returns true if event was dispatched by the user agent, and false otherwise. */
+    readonly isTrusted: boolean;
+    /** @deprecated */
+    returnValue: boolean;
+    /** @deprecated */
+    readonly srcElement: EventTarget | null;
+    /** Returns the object to which event is dispatched (its target). */
+    readonly target: EventTarget | null;
+    /** Returns the event's timestamp as the number of milliseconds measured relative to the time origin. */
+    readonly timeStamp: DOMHighResTimeStamp;
+    /** Returns the type of event, e.g. "click", "hashchange", or "submit". */
+    readonly type: string;
+    /** Returns the invocation target objects of event's path (objects on which listeners will be invoked), except for any nodes in shadow trees of which the shadow root's mode is "closed" that are not reachable from event's currentTarget. */
+    composedPath(): EventTarget[];
+    /** @deprecated */
+    initEvent(type: string, bubbles?: boolean, cancelable?: boolean): void;
+    /** If invoked when the cancelable attribute value is true, and while executing a listener for the event with passive set to false, signals to the operation that caused event to be dispatched that it needs to be canceled. */
+    preventDefault(): void;
+    /** Invoking this method prevents event from reaching any registered event listeners after the current one finishes running and, when dispatched in a tree, also prevents event from reaching any other objects. */
+    stopImmediatePropagation(): void;
+    /** When dispatched in a tree, invoking this method prevents event from reaching any objects other than the current object. */
+    stopPropagation(): void;
+    readonly AT_TARGET: number;
+    readonly BUBBLING_PHASE: number;
+    readonly CAPTURING_PHASE: number;
+    readonly NONE: number;
+}
+declare var Event: {
+    prototype: Event;
+    new (type: string, eventInitDict?: EventInit): Event;
+    readonly AT_TARGET: number;
+    readonly BUBBLING_PHASE: number;
+    readonly CAPTURING_PHASE: number;
+    readonly NONE: number;
+};
+interface EventListener {
+}
+interface EventListenerObject {
+    handleEvent(object: Event): void;
+}
+/** EventTarget is a DOM interface implemented by objects that can receive events and may have listeners for them. */
+interface EventTarget {
+    /**
+     * Appends an event listener for events whose type attribute value is type. The callback argument sets the callback that will be invoked when the event is dispatched.
+     *
+     * The options argument sets listener-specific options. For compatibility this can be a boolean, in which case the method behaves exactly as if the value was specified as options's capture.
+     *
+     * When set to true, options's capture prevents callback from being invoked when the event's eventPhase attribute value is BUBBLING_PHASE. When false (or not present), callback will not be invoked when event's eventPhase attribute value is CAPTURING_PHASE. Either way, callback will be invoked if event's eventPhase attribute value is AT_TARGET.
+     *
+     * When set to true, options's passive indicates that the callback will not cancel the event by invoking preventDefault(). This is used to enable performance optimizations described in § 2.8 Observing event listeners.
+     *
+     * When set to true, options's once indicates that the callback will only be invoked once after which the event listener will be removed.
+     *
+     * If an AbortSignal is passed for options's signal, then the event listener will be removed when signal is aborted.
+     *
+     * The event listener is appended to target's event listener list and is not appended if it has the same type, callback, and capture.
+     */
+    addEventListener(type: string, callback: EventListenerOrEventListenerObject | null, options?: AddEventListenerOptions | boolean): void;
+    /** Dispatches a synthetic event event to target and returns true if either event's cancelable attribute value is false or its preventDefault() method was not invoked, and false otherwise. */
+    dispatchEvent(event: Event): boolean;
+    /** Removes the event listener in target's event listener list with the same type, callback, and options. */
+    removeEventListener(type: string, callback: EventListenerOrEventListenerObject | null, options?: EventListenerOptions | boolean): void;
+}
+declare var EventTarget: {
+    prototype: EventTarget;
+    new (): EventTarget;
 };
 /**
  * This Web Crypto API interface provides a number of low-level cryptographic functions. It is accessed via the Crypto.subtle properties available in a window context (via Window.crypto).
@@ -216,6 +333,8 @@ declare function btoa(data: string): string;
 type AlgorithmIdentifier = Algorithm | string;
 type BigInteger = Uint8Array;
 type BufferSource = ArrayBufferView | ArrayBuffer;
+type DOMHighResTimeStamp = number;
+type EventListenerOrEventListenerObject = EventListener | EventListenerObject;
 type HashAlgorithmIdentifier = AlgorithmIdentifier;
 type NamedCurve = string;
 type KeyFormat = "jwk" | "pkcs8" | "raw" | "spki";
